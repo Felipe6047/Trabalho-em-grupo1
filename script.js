aa32b2fe8872aff6cc74ca8dfdf5c430db05ee99
Barusu <ff063836421@gmail.com>
2026-02-03 14:59:58 -0300

    feat: começando do zero

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 alert("Sua privacidade é nossa prioridade. Adotamos procedimentos rigorosos de segurança para proteger seus dados contra acessos não autorizados.");
 
+const form = document.getElementById
+
+
+
+// window.onload = Informaçõesparacontato;
+
+// function enviar() {('enviar').value;
+// }
+
+// function enviarDados() {
+  
+//   const input = document.getElementById('').value;
+//   const numero = parseInt(input);
+
+//   function carregarDados() {
+//     const element = document.getElementById('...');
+//     const dados = localStorage.getItem('...');
+
+
+//     if (dados) {
+//       const primos = JSON.parse(dados);
+//       element.innerHTML = dados;
+
+//     }
+//   }
+// }
+// function limpar() {
+//   localStorage.clear();
+//   document.getElementById('...').innerHTML = '';
+//   document.getElementById('...').value = '';
+// }
